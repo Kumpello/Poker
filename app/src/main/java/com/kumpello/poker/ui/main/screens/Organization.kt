3eabf406d2d74449289499ca369362ca957ea469
Kumpel <bleidd04@gmail.com>
2023-03-26 17:56:31 +0200

    -wip

--- app/src/main/java/com/kumpello/poker/ui/main/screens/Organization.kt
+++ app/src/main/java/com/kumpello/poker/ui/main/screens/Organization.kt
@@ -1,2 +1,38 @@
 package com.kumpello.poker.ui.main.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kumpello.poker.data.model.ID
+
+@Composable
+fun Organization(id: ID, name: String, membersCount: Int) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(name)
+        Text(membersCount.toString())
+        IconButton(
+            onClick = {
+
+            },
+            modifier = Modifier
+                .height(50.dp)
+        ) {
+            Icon(
+                modifier = Modifier.size(size = 50.dp),
+                imageVector = Icons.Outlined.Delete,
+                contentDescription = "",
+                tint = androidx.compose.ui.graphics.Color.Red
+            )
+        }
+    }
+}
--- app/src/main/java/com/kumpello/poker/ui/main/screens/Organizations.kt
+++ app/src/main/java/com/kumpello/poker/ui/main/screens/Organizations.kt
@@ -1,6 +1,9 @@
 package com.kumpello.poker.ui.main.screens
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -12,9 +15,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kumpello.poker.data.model.OrganizationData
 import com.kumpello.poker.ui.theme.PokerTheme
+import java.util.Collections
 
 @Composable
-fun Organizations() {
+fun Organizations(organizations: List<OrganizationData>) {
     val mContext = LocalContext.current
 
     Column(
@@ -28,7 +32,15 @@ fun Organizations() {
             Spacer(Modifier.size(20.dp))
             FindOrganizationsButton()
         }
-
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            contentPadding = PaddingValues(16.dp),
+            state = rememberLazyListState(),
+        ) {
+            items(organizations) { organization ->
+                Organization(organization.id, organization.name, organization.members.size)
+            }
+        }
     }
 }
 
@@ -60,15 +72,10 @@ fun FindOrganizationsButton() {
     }
 }
 
-@Composable
-fun OrganizationsList(organizations: List<OrganizationData>) {
-
-}
-
 @Preview(showBackground = true)
 @Composable
 fun OrganizationsPreview() {
     PokerTheme {
-        Organizations()
+        Organizations(Collections((2137, "dupa", 5)))
     }
 }
